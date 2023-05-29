9144289ab427c29434d5a7dfac59cd435c5a0608
TenTraicion <tentraicion@gmail.com>
2023-05-29 23:45:58 +0600

    END: Basic Drill

--- functions.php
+++ functions.php
@@ -11,8 +11,8 @@ $password = $_POST['password'];
 $username = mysqli_real_escape_string($connection, $username);   
 $password = mysqli_real_escape_string($connection, $password);
    
-$hashFormat = "$2y$10$"; 
-$salt = "iusesomecrazystrings22";
+$hashFormat = "$2y$16$"; 
+$salt = "tentraicion00platinamo";
 $hashF_and_salt = $hashFormat . $salt;
 $password = crypt($password,$hashF_and_salt);   
     
